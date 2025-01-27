c049e73267189ff4cb5717de54b79fcee4192e0d
cassLaffan <poisonberryvines@gmail.com>
2025-01-27 10:19:54 -0500

    Adding a try/except in the client code

--- Week_4/Client.py
+++ Week_4/Client.py
@@ -8,15 +8,22 @@ if __name__ == "__main__":
 	# Client setup
 	# For explanation, see Server file
 	client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	# Connects to server listening
-	client_socket.connect(('localhost', 8000))
 
-	# Send message to server
-	client_socket.send(b"Hello from client!")
+	# I've wrapped the client connection in a try/except block, just in case
+	# the server isn't on!
+	try:
+		# Connects to server listening
+		client_socket.connect(('localhost', 8000))
 
-	# Receive response from server
-	response = client_socket.recv(1024)
-	print(f"Server says: {response.decode()}")
+		# Send message to server
+		client_socket.send(b"Hello from client!")
+
+		# Receive response from server
+		response = client_socket.recv(1024)
+		print(f"Server says: {response.decode()}")
+
+	except socket.error:
+		print("Socket isn't listening!")
 
 	# Close the connection
 	client_socket.close()
