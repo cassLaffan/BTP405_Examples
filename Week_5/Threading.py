7a04dc8651a31c9ee2a52bb5cd1ac10e28546bc4
cassLaffan <poisonberryvines@gmail.com>
2025-01-31 19:40:57 -0500

    Adding an example

---
+++ Week_5/Threading.py
@@ -0,0 +1,30 @@
+import threading
+import time
+
+'''
+In this file, we take a look at threading in python!
+Remember, threads are a lightweight sub-unit of a process.
+These share memory with other threads in the same process!
+'''
+
+def download_file(file_name):
+	print(f"Downloading {file_name}...")
+	# Simulate download time
+	time.sleep(5)
+	print(f"Finished downloading {file_name}")
+
+if __name__ == "__main__":
+	# Create threads
+	threads = []
+	files = ["file1.zip", "file2.zip", "file3.zip"]
+
+	for file in files:
+		thread = threading.Thread(target=download_file, args=(file,))
+		threads.append(thread)
+		thread.start()
+
+	# Wait for all threads to finish
+	for thread in threads:
+		thread.join()
+
+	print("All downloads complete!")
